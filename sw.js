f59c42416d180b2199e0f113d910c19e803982f7
gfjdh1 <2556123498@qq.com>
2024-09-03 12:44:41 +0800

    Site updated: 2024-09-03 12:44:41

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1725122730259";
+const VERSION = "1725338677708";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
