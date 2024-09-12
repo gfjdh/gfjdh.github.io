f8d9e7b6410e8ac72d6900e6e4d262723f671c6b
gfjdh1 <2556123498@qq.com>
2024-09-12 23:05:45 +0800

    Site updated: 2024-09-12 23:05:45

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1726151601781";
+const VERSION = "1726153541899";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
